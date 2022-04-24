e9418ca3cc9e114a3a1a40654ce3345d04e66b82
Jeffrey Luo <jeffrey.luo18@gmail.com>
2022-04-24 14:42:21 -0400

    finalize

--- script.js
+++ script.js
@@ -7,28 +7,62 @@ function readTextFile(file, callback) {
         if (rawFile.readyState === 4 && rawFile.status == "200") {
             callback(rawFile.responseText);
         }
-    }
+    } 
     rawFile.send(null);
 }
 
+ALL_DATA_LOADED = false;
+
+var top50char_data;
+var char_to_definition_data;
+var char_override_dict_data;
+
 readTextFile("https://monicalin22.github.io/chinese-names/data/top50char.json", function(text){
-	var top50char_data = JSON.parse(text);
+	top50char_data = JSON.parse(text);
 	
 	readTextFile("https://monicalin22.github.io/chinese-names/data/char_to_definition.json", function(text) {
-		var char_to_definition_data = JSON.parse(text);
+		char_to_definition_data = JSON.parse(text);
 		
 		readTextFile("https://monicalin22.github.io/chinese-names/data/char_override_dict.json", function(text) {
-			var char_override_dict_data = JSON.parse(text);
-			
+			char_override_dict_data = JSON.parse(text);
+			ALL_DATA_LOADED = true;
 			createVisual(top50char_data, char_to_definition_data, char_override_dict_data);
 		});
 	});
 });
 
+var decade2 = 1950;
 
-const decade2 = 1960;
+update_decade_in_html = () => {
+	document.getElementById("output").innerHTML = decade2;
+}
+
+document.addEventListener("DOMContentLoaded", evt => {
+	update_decade_in_html();
+	
+	decade_select_slider = document.getElementById("decade-selector");
+	
+	decade_select_slider.addEventListener("input", evt => {
+		year_select   = decade_select_slider.value;
+		decade_select = Math.floor(year_select / 10) * 10;
+		
+		if (ALL_DATA_LOADED && (decade_select != decade2)) {
+			let svg = d3.selectAll("#decade-prevalence-vis");
+			svg.selectAll("*").remove();
+			
+			// Create decade prevalance visualization
+			decade2 = decade_select;
+			update_decade_in_html();
+			createVisual(top50char_data, char_to_definition_data, char_override_dict_data);
+			
+			console.log("Decade selected: " + decade_select);
+			console.log("Visual created/updated!");	
+		}	
+	});
+});
 
 
+// Create the decades-prevalence visualization
 createVisual = function(data, char_to_definition, char_override_dict) {
 
 	let char1950 = new Set(data.map(obj => obj["char.all.1950"]));
@@ -73,13 +107,14 @@ createVisual = function(data, char_to_definition, char_override_dict) {
 	const num_height = 5;
 
 	// Display settings
-	const windowHeight  = 400;
-	const windowWidth   = 600;
-	const MULT          = 0.96;
-	const squareHeight  = windowHeight / num_height * MULT;
-	const squareWidth   = windowWidth / num_width   * MULT;
-
-	const svg = d3.create("svg").attr('viewBox', [0, 0, windowWidth, windowHeight]);
+	const windowHeight = 400 * 0.20;
+	const windowWidth  = 600 * 0.20;
+	const MULT         = 0.96;
+	const squareHeight = windowHeight / num_height * MULT;
+	const squareWidth  = windowWidth  / num_width  * MULT;
+	
+	// const svg = d3.create("svg").attr('viewBox', [0, 0, windowWidth, windowHeight]);
+	const svg = d3.selectAll("#decade-prevalence-vis").attr('viewBox', [0, 0, windowWidth, windowHeight]);
 
 	let dict = {};
 
@@ -87,24 +122,27 @@ createVisual = function(data, char_to_definition, char_override_dict) {
 	let y = d3.scaleLinear().domain([0, num_height]).range([0, windowHeight]);
 
 	// Text settings
-	let font_size         = "1.50rem";
+	let font_size         = "0.25rem";
 
-	const x_text_offset   = 15;
-	const y_text_offset   = 44;
+	const x_text_offset   = 4; // 15;
+	const y_text_offset   = 9; // 44;
+	
+	const x_def_offset    = -2;
+	const y_def_offset    = 4;
 
 	// 1950 characters
 	let chars1950 = post[0];
 
 	function get_male_to_total_ratio(char, ind) {
-	return post_ratios[ind][char];
+		return post_ratios[ind][char];
 	}
 
-	let k     = 0;
+	let k = 0;
 	for (let row = 0; row < num_height; row++) {
 		for (let col = 0; col < num_width; col++) {
 			let ind   = (-1950 + decade2) / 10;
 			let chars = post[ind];
-			let char = Array.from(chars)[k];
+			let char_ = Array.from(chars)[k];
 
 			let g    = svg.append("g");  
 			let rect = g
@@ -115,7 +153,7 @@ createVisual = function(data, char_to_definition, char_override_dict) {
 				.attr("height", squareHeight)
 				.attr("fill",  "#ff69b4");
 
-			let ratio = get_male_to_total_ratio(char, ind);
+			let ratio = get_male_to_total_ratio(char_, ind);
 
 			let rect2 = null;
 			if (ratio != null) {
@@ -140,27 +178,26 @@ createVisual = function(data, char_to_definition, char_override_dict) {
 			//let defn = char_to_definition[Array.from(chars1950)[k]].split(";")[0].split(",")[0];
 			let defn = !char_override_dict.hasOwnProperty(Array.from(chars1950)[k]) ? char_to_definition[Array.from(chars1950)[k]].split(";")[0].split(",")[0] : String(char_override_dict[Array.from(chars1950)[k]]);
 
-			let definition_size   = "0.50rem";
-			if (defn.length >= 15) {definition_size = "0.35rem"};
-			if (defn.length <= 5)  {definition_size = "0.65rem"};
+			let definition_size   = "0.13rem";
+			if (defn.length >= 15) {definition_size = "0.07rem"};
+			if (defn.length <= 5)  {definition_size = "0.16rem"};
 
 			// Definition text
 			let text2 = g
 				.append("text")
-				.attr("x", x(col) + x_text_offset - 7)
-				.attr("y", y(row) + y_text_offset + 17)
+				.attr("x", x(col) + x_text_offset + x_def_offset)
+				.attr("y", y(row) + y_text_offset + y_def_offset)
 				.style("font-size", definition_size)
 				.style("fill", "#FFFFFF")
 				.text(defn);
 
-			if (!(chars1950.has(char))) { // If doesn't exist, make it partly transparent
+			if (!(chars1950.has(char_))) { // If doesn't exist, make it partly transparent
 				rect.attr("opacity", 0.15);
 				text.style("fill", "#BBBBBB");
 				text2.style("fill", "#BBBBBB");
 			}
-
+			
 			k++;
 		}
 	}
-
 }
